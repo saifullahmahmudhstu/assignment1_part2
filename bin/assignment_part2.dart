66d95f38de7e7daa5bdc6713bc6bc4b02c17bd11
Rimon <saifullahmahmud.hstu@gmail.com>
2022-02-14 19:44:07 +0600

    dynamic input

---
+++ bin/assignment_part2.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+
+  //for a
+
+  int p = int.parse(stdin.readLineSync()!);
+  int q = int.parse(stdin.readLineSync()!);
+  int r = int.parse(stdin.readLineSync()!);
+  
+  int total1 = p + q * r ;
+
+  print('$p + $q * $r = $total1');
+
+
+
+
+
+}
